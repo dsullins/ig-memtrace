2f9a6c8b569771ff476e2bb1935912b83747eafc
NVIDIA.COM\xnie <xnie@nvidia.com>
2018-02-09 18:37:53 +0800

    Use HashSet to optimize the performance of tool.

--- tool/MemTrace/TraceMeta.cs
+++ tool/MemTrace/TraceMeta.cs
@@ -43,7 +43,7 @@ namespace MemTrace
     /// <summary>
     /// Symbols that appear in the trace file.
     /// </summary>
-    public SortedSet<ulong> Symbols { get; internal set; }
+    public HashSet<ulong> Symbols { get; internal set; }
     /// <summary>
     /// Position in the file where the footer lives
     /// </summary>
@@ -93,7 +93,7 @@ namespace MemTrace
     internal TraceMeta()
     {
       Status = TraceStatus.Ready;
-      Symbols = new SortedSet<ulong>();
+      Symbols = new HashSet<ulong>();
       ResolvedSymbols = new Dictionary<ulong, SymbolInfo>();
       m_Marks = new List<TraceMark>();
       PlatformName = "Unknown";
@@ -161,7 +161,7 @@ namespace MemTrace
 
           {
             int symcount = br.ReadInt32();
-            Symbols = new SortedSet<ulong>();
+            Symbols = new HashSet<ulong>();
             for (int i = 0; i < symcount; ++i)
             {
               var addr = br.ReadUInt64();
